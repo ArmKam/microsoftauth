e9b7c29b1b390d61fc3299fded96b3a43bb5b99b
Yepremyan <kamoep84@gmail.com>
2021-02-11 01:17:45 +0100

    created Microsoft auth token app

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const msalConfig = {
   auth: {
     clientId:'cf7955df-814d-415a-a1df-da385c4f4a16',
     authority: 'https://login.microsoftonline.com/common',
-    redirectUri: 'http://localhost:8081'
+    redirectUri: 'http://localhost:8080'
   }
 };
 
